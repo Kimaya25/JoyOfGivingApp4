7b6c03c24ab2fb0deab45b4fe044d710447c05e0
Kimaya25 <65022299+Kimaya25@users.noreply.github.com>
2020-12-28 17:38:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './Screens/WelcomeScreen';
+import AppTabNavigator from './Components/AppTabNavigator';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+export default class App extends React.Component {
+  render(){
+  return (
+    <View style={styles.container}>
+     <TabContainer/>
+    </View>
+  );
+}
+}
+const SwitchNavigator = createSwitchNavigator({
+  AppTabNavigator : {screen: AppTabNavigator},
+  WelcomeScreen : {screen: WelcomeScreen}
+
+})
+const TabContainer = createAppContainer(
+  SwitchNavigator
+)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
